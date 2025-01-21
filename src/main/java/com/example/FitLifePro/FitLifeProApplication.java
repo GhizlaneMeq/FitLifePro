838956d289d6ddd98eff08e286aca8c175d875c1
GhizlaneMeq <meqdarghizlane@gmail.com>
2025-01-21 15:07:24 +0100

    creation du projet

---
+++ src/main/java/com/example/FitLifePro/FitLifeProApplication.java
@@ -0,0 +1,13 @@
+package com.example.FitLifePro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitLifeProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitLifeProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FitLifePro/FitLifeProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FitLifePro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitLifeProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
